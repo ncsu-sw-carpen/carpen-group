c525252e04a85d9a149199a028614127612fb115
xian@oryza <code@x14n.org>
2015-01-31 00:05:36 -0700

    Updated .gitignore.  Minor edits to instructions and code.

--- wk01.talkfun.py
+++ wk01.talkfun.py
@@ -2,10 +2,13 @@
 ## Comments are lines starting with #
 ## except the very first line, which is special
 
-def talk(say="Hello", to="World"):
-    '''This is a docstring'''
-    retVar = say + to
+def talk(say="Hello", to="World", sep=", "):
+    '''## This is the docstring of function talk. \n## This function takes 3 arguments: say (string), to (string), sep (string) .'''
+    retVar = say + sep + to
     return retVar
 
 print talk()
-print dir(talk)
+
+## extra stuff for later
+#print dir(talk)
+#print talk.__doc__
