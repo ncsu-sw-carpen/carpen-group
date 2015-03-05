b8f137d5802e239cc3d2938617f3dca4adbcbd5d
scsuarez <s.c.suarez@mac.com>
2015-03-05 15:57:18 -0500

    playing around with wk03.data.py file

---
+++ Sam/wk03.data.py
@@ -0,0 +1,54 @@
+import re, csv, pdb
+
+## csv of experimental data
+dataFile = 'data/swc.chemA.csv'
+filehandle = open(dataFile)
+
+## read the csv into a dictionary-like object
+## see https://docs.python.org/2/library/csv.html
+geneExpr = csv.DictReader(filehandle)
+
+## look at one line
+row01 = geneExpr.next()
+print '\nData columns: ' 
+print row01.keys()
+print '\nData values: ' 
+print row01.values()
+print '\nPrinting gene value: ' + row01['gene']
+
+## We can access values using a named variable 
+colName = 'Annotation'
+## this is "fancy" string formatting in python
+print 'Row01 has value %s for column %s\n' % (row01[colName], colName) 
+
+## try to put "hard-coded" values in named variables
+nlim = 5
+mysum = 0
+ii = 0
+print "Printing %d rows of %s" % (nlim, colName)
+## for each row 
+for thisRow in geneExpr:
+    #print thisRow[colName]
+    mysum = mysum + float(thisRow['value_1'])
+    #ii += 1
+    #if (ii > nlim ): 
+        ## exit the for loop
+        ##break
+print 'the sum is %f' % mysum
+
+## open debugging, move this chunk around as needed
+pdb.set_trace()
+
+raw_input('\nHit enter to continue\n')
+parenPatt = '\((\w+)\)'
+ii = 0
+print "Row\t\tgroup(0)\t\tgroup(1)"
+for thisRow in geneExpr:
+    ii += 1
+    ## search for, e.g. 2015-01-01 00:00:01
+    gene = re.search(parenPatt, thisRow['Annotation'])
+    ## if there's *any* match
+    if (gene):
+        #print line
+        ## print the pattern in the second group of parens 
+        print "%d\t\t%s\t\t%s" % ( ii, gene.group(0), gene.group(1))
