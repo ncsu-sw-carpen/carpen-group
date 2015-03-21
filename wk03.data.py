9649c1996a015e0b831ed8be45ca27b64d32f7e9
Emily Griffiths <ecg@Emilys-MacBook-Pro-2.local>
2015-03-21 00:44:29 -0400

    In the main carpen-group directory working on wk03.data.py. Fixed raw_input bug. Put all print statements back to python2 style.

--- wk03.data.py
+++ wk03.data.py
@@ -13,33 +13,33 @@ geneExpr = csv.DictReader(filehandle)
 
 ## look at one line
 row01 = geneExpr.next()
-print ('\nData columns: ')
-print (row01.keys())
-print ('\nData values: ')
-print (row01.values())
-print ('\nPrinting gene value: ' + row01['gene'])
+print '\nData columns: '
+print row01.keys()
+print '\nData values: '
+print row01.values()
+print '\nPrinting gene value: ' + row01['gene']
 
 ## We can access values using a named variable 
 colName = 'Annotation'
 ## this is "fancy" string formatting in python
-print ('Row01 has value %s for column %s\n' % (row01[colName], colName) )
+print 'Row01 has value %s for column %s\n' % (row01[colName], colName)
 
 ## try to put "hard-coded" values in named variables
 nlim = 5
 ii = 0
-print ("Printing %d rows of %s" % (nlim, colName))
+print "Printing %d rows of %s" % (nlim, colName)
 ## for each row 
 for thisRow in geneExpr:
-    print (thisRow[colName])
+    print thisRow[colName]
     ii += 1
     if (ii > nlim ): 
         ## exit the for loop
         break
 
-raw_input('\nHit enter to continue\n')
+raw_input=('\nHit enter to continue\n')
 parenPatt = '\((\w+)\)'
 ii = 0
-print ("Row\t\tgroup(0)\t\tgroup(1)")
+print "Row\t\tgroup(0)\t\tgroup(1)"
 for thisRow in geneExpr:
     ii += 1
     ## search for, e.g. 2015-01-01 00:00:01
@@ -48,4 +48,4 @@ for thisRow in geneExpr:
     if (gene):
         #print line
         ## print the pattern in the second group of parens 
-        print ("%d\t\t%s\t\t%s" % ( ii, gene.group(0), gene.group(1)))
+        print "%d\t\t%s\t\t%s" % ( ii, gene.group(0), gene.group(1))
